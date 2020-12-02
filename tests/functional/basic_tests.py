9ee016db8e58a3a822dd9f372f35d10502b5a649
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2020-12-02 15:42:00 +0200

    Log diagnostic info if fields missing from an API response

--- tests/functional/basic_tests.py
+++ tests/functional/basic_tests.py
@@ -1,5 +1,6 @@
 import os
 import urllib.request
+import logging
 import tempfile
 
 from geoprofile.app import app
@@ -7,7 +8,6 @@ from geoprofile.app import app
 # Setup/Teardown
 
 _tempdir: str = ""
-
 
 def setup_module():
     print(f" == Setting up tests for {__name__}")
@@ -28,8 +28,18 @@ def teardown_module():
     print(f" == Tearing down tests for {__name__}")
     pass
 
-# Tests
 
+def _check_all_fields_are_present(expected, r, api_path):
+    '''Check that all expected fields are present in a JSON response object (only examines top-level fields)
+    '''
+    missing = expected.difference(r.keys())
+    if missing:
+        logging.error('%s: the response is expected to also have these fields: %s', api_path, missing)
+        assert False, 'The response is missing some fields'
+
+#
+# Tests
+#
 
 def test_get_documentation_1():
     with app.test_client() as client:
@@ -38,41 +48,40 @@ def test_get_documentation_1():
         r = res.get_json()
         assert not (r.get('openapi') is None)
 
-
 def test_profile_netcdf_file_input_prompt():
     url = 'https://www.unidata.ucar.edu/software/netcdf/examples/test_echam_spectral-deflated.nc'
     tmp_file_path = os.path.join(_tempdir, 'sample_netcdf.nc')
     urllib.request.urlretrieve(url, tmp_file_path)
     data = {'resource': (open(tmp_file_path, 'rb'), 'sample_netcdf.nc')}
+    path_to_test = '/profile/file/netcdf'
     with app.test_client() as client:
         # Test if it fails when no file is submitted
-        res = client.post('/profile/file/netcdf', content_type='multipart/form-data')
+        res = client.post(path_to_test, content_type='multipart/form-data')
         assert res.status_code == 400
         # Test if it succeeds when a file is submitted
-        res = client.post('/profile/file/netcdf', data=data, content_type='multipart/form-data')
+        res = client.post(path_to_test, data=data, content_type='multipart/form-data')
         assert res.status_code in [200, 202]
         # Test if it returns the expected fields
         r = res.get_json()
         expected_fields = {'mbr', 'metadata', 'variables_list', 'dimensions_size', 'temporal_extent', 'dimensions_list',
                            'variables_properties', 'no_data_values', 'statistics', 'variables_size', 'sample',
                            'dimensions_properties'}
-        assert set(r.keys()) == expected_fields
-
+        _check_all_fields_are_present(expected_fields, r, path_to_test);
 
 def test_profile_netcdf_file_input_deferred():
     url = 'https://www.unidata.ucar.edu/software/netcdf/examples/test_echam_spectral-deflated.nc'
     tmp_file_path = os.path.join(_tempdir, 'sample_netcdf.nc')
     urllib.request.urlretrieve(url, tmp_file_path)
     data = {'resource': (open(tmp_file_path, 'rb'), 'sample_netcdf.nc')}
+    path_to_test = '/profile/file/netcdf'
     with app.test_client() as client:
         # Test if it succeeds when a file is submitted with deferred processing
-        res = client.post('/profile/file/netcdf', data=data, content_type='multipart/form-data')
+        res = client.post(path_to_test, data=data, content_type='multipart/form-data')
         assert res.status_code in [200, 202]
         # Test if it returns the expected fields
         r = res.get_json()
         expected_fields = {'endpoint', 'status', 'ticket'}
-        assert set(r.keys()) == expected_fields
-
+        _check_all_fields_are_present(expected_fields, r, path_to_test);
 
 def test_profile_raster_file_input_prompt():
     url = 'http://even.rouault.free.fr' \
@@ -80,19 +89,19 @@ def test_profile_raster_file_input_prompt():
     tmp_file_path = os.path.join(_tempdir, 'sample_512.tif')
     urllib.request.urlretrieve(url, tmp_file_path)
     data = {'resource': (open(tmp_file_path, 'rb'), 'sample_512.tif')}
+    path_to_test = '/profile/file/raster'
     with app.test_client() as client:
         # Test if it fails when no file is submitted
-        res = client.post('/profile/file/raster', content_type='multipart/form-data')
+        res = client.post(path_to_test, content_type='multipart/form-data')
         assert res.status_code == 400
         # Test if it succeeds when a file is submitted
-        res = client.post('/profile/file/raster', data=data, content_type='multipart/form-data')
+        res = client.post(path_to_test, data=data, content_type='multipart/form-data')
         assert res.status_code in [200, 202]
         # Test if it returns the expected fields
         r = res.get_json()
         expected_fields = {'cog', 'color_interpetation', 'crs', 'datatypes', 'histogram', 'info', 'mbr', 'noDataValue',
                            'number_of_bands', 'resolution', 'statistics'}
-        assert set(r.keys()) == expected_fields
-
+        _check_all_fields_are_present(expected_fields, r, path_to_test);
 
 def test_profile_raster_file_input_deferred():
     url = 'http://even.rouault.free.fr' \
@@ -100,30 +109,31 @@ def test_profile_raster_file_input_deferred():
     tmp_file_path = os.path.join(_tempdir, 'sample_512.tif')
     urllib.request.urlretrieve(url, tmp_file_path)
     data = {'resource': (open(tmp_file_path, 'rb'), 'sample_512.tif')}
+    path_to_test = '/profile/file/raster'
     with app.test_client() as client:
         # Test if it succeeds when a file is submitted with deferred processing
-        res = client.post('/profile/file/raster', data=data, content_type='multipart/form-data')
+        res = client.post(path_to_test, data=data, content_type='multipart/form-data')
         assert res.status_code in [200, 202]
         # Test if it returns the expected fields
         r = res.get_json()
         expected_fields = {'endpoint', 'status', 'ticket'}
-        assert set(r.keys()) == expected_fields
-
+        _check_all_fields_are_present(expected_fields, r, path_to_test);
 
 # def test_profile_vector_file_input():
 #     url = 'https://download.geofabrik.de/europe/great-britain/wales-latest-free.shp.zip'
 #     tmp_file_path = os.path.join(_tempdir, 'wales-latest-free.shp.zip')
 #     urllib.request.urlretrieve(url, tmp_file_path)
 #     data = {'resource': (open(tmp_file_path, 'rb'), 'wales-latest-free.shp.zip')}
+#     path_to_test = '/profile/file/vector' 
 #     with app.test_client() as client:
-#         res = client.post('/profile/file/vector', content_type='multipart/form-data')
+#         res = client.post(path_to_test, content_type='multipart/form-data')
 #         assert res.status_code == 400
-#         res = client.post('/profile/file/vector', data=data, content_type='multipart/form-data')
+#         res = client.post(path_to_test, data=data, content_type='multipart/form-data')
 #         assert res.status_code in [200, 202]
 #         r = res.get_json()
 #         expected_fields = {'statistics', 'distribution', 'quantiles', 'recurring', 'distinct', 'datatypes', 'thumbnail',
 #                            'attributes', 'convex_hull', 'crs', 'featureCount', 'mbr', 'count'}
-#         assert set(r.keys()) == expected_fields
+#        _check_all_fields_are_present(expected_fields, r, path_to_test);
 #     os.remove(tmp_file_path)
 
 
@@ -132,36 +142,36 @@ def test_profile_netcdf_path_input_prompt():
     tmp_file_path = os.path.join(_tempdir, 'sample_netcdf.nc')
     urllib.request.urlretrieve(url, tmp_file_path)
     data = {'resource': tmp_file_path}
+    path_to_test = '/profile/path/netcdf'
     with app.test_client() as client:
         # Test if it fails when no file is submitted
-        res = client.post('/profile/path/netcdf', content_type='application/x-www-form-urlencoded')
+        res = client.post(path_to_test, content_type='application/x-www-form-urlencoded')
         assert res.status_code == 400
         # Test if it succeeds when a file is submitted
-        res = client.post('/profile/path/netcdf', data=data, content_type='application/x-www-form-urlencoded')
+        res = client.post(path_to_test, data=data, content_type='application/x-www-form-urlencoded')
         assert res.status_code in [200, 202]
         # Test if it returns the expected fields
         r = res.get_json()
         expected_fields = {'mbr', 'metadata', 'variables_list', 'dimensions_size', 'temporal_extent', 'dimensions_list',
                            'variables_properties', 'no_data_values', 'statistics', 'variables_size', 'sample',
                            'dimensions_properties'}
-        assert set(r.keys()) == expected_fields
-
+        _check_all_fields_are_present(expected_fields, r, path_to_test);
 
 def test_profile_netcdf_path_input_deferred():
     url = 'https://www.unidata.ucar.edu/software/netcdf/examples/test_echam_spectral-deflated.nc'
     tmp_file_path = os.path.join(_tempdir, 'sample_netcdf.nc')
     urllib.request.urlretrieve(url, tmp_file_path)
     data = {'resource': tmp_file_path}
+    path_to_test = '/profile/path/netcdf'
     with app.test_client() as client:
         data['response'] = 'deferred'
         # Test if it succeeds when a file is submitted with deferred processing
-        res = client.post('/profile/path/netcdf', data=data, content_type='application/x-www-form-urlencoded')
+        res = client.post(path_to_test, data=data, content_type='application/x-www-form-urlencoded')
         assert res.status_code in [200, 202]
         # Test if it returns the expected fields
         r = res.get_json()
         expected_fields = {'endpoint', 'status', 'ticket'}
-        assert set(r.keys()) == expected_fields
-
+        _check_all_fields_are_present(expected_fields, r, path_to_test);
 
 def test_profile_raster_path_input_prompt():
     url = 'http://even.rouault.free.fr' \
@@ -169,19 +179,19 @@ def test_profile_raster_path_input_prompt():
     tmp_file_path = os.path.join(_tempdir, 'sample_512.tif')
     urllib.request.urlretrieve(url, tmp_file_path)
     data = {'resource': tmp_file_path}
+    path_to_test = '/profile/path/raster'
     with app.test_client() as client:
         # Test if it fails when no file is submitted
-        res = client.post('/profile/path/raster', content_type='application/x-www-form-urlencoded')
+        res = client.post(path_to_test, content_type='application/x-www-form-urlencoded')
         assert res.status_code == 400
         # Test if it succeeds when a file is submitted
-        res = client.post('/profile/path/raster', data=data, content_type='application/x-www-form-urlencoded')
+        res = client.post(path_to_test, data=data, content_type='application/x-www-form-urlencoded')
         assert res.status_code in [200, 202]
         # Test if it returns the expected fields
-        r1 = res.get_json()
+        r = res.get_json()
         expected_fields = {'cog', 'color_interpetation', 'crs', 'datatypes', 'histogram', 'info', 'mbr', 'noDataValue',
                            'number_of_bands', 'resolution', 'statistics'}
-        assert set(r1.keys()) == expected_fields
-
+        _check_all_fields_are_present(expected_fields, r, path_to_test);
 
 def test_profile_raster_path_input_deferred():
     url = 'http://even.rouault.free.fr' \
@@ -189,12 +199,14 @@ def test_profile_raster_path_input_deferred():
     tmp_file_path = os.path.join(_tempdir, 'sample_512.tif')
     urllib.request.urlretrieve(url, tmp_file_path)
     data = {'resource': tmp_file_path}
+    path_to_test = '/profile/path/raster'
     with app.test_client() as client:
         data['response'] = 'deferred'
         # Test if it succeeds when a file is submitted with deferred processing
-        res = client.post('/profile/path/raster', data=data, content_type='application/x-www-form-urlencoded')
+        res = client.post(path_to_test, data=data, content_type='application/x-www-form-urlencoded')
         assert res.status_code in [200, 202]
         # Test if it returns the expected fields
-        r2 = res.get_json()
+        r = res.get_json()
         expected_fields = {'endpoint', 'status', 'ticket'}
-        assert set(r2.keys()) == expected_fields
+        _check_all_fields_are_present(expected_fields, r, path_to_test);
+
