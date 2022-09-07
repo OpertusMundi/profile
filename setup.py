91cb0c5a26274f02bf30e60a195e67662c7cc296
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2022-09-07 19:42:24 +0300

    Bump version to 2.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='geoprofile',
-    version='1.0.0',
+    version='2.0.0',
     description='Raster and vector files profiling service',
     license='MIT',
     packages=setuptools.find_packages(exclude=('tests*',)),
